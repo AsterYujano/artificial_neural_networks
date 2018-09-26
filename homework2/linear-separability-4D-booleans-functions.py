07639e3cdb76464c48830632ea15c3772076b77e
François <asteryujano@gmail.com>
2018-09-26 13:49:09 +0100

    exo2 - Add threshold, 10 times, activation fct updated

--- homework2/linear-separability-4D-booleans-functions.py
+++ homework2/linear-separability-4D-booleans-functions.py
@@ -4,7 +4,7 @@ import sys
 from matplotlib import pyplot as plt
 
 datasets = [
-	#   x1,x2,x3,y
+	#   x1,x2,x3,x4
 		[-1,-1,-1,-1],
 		[1,-1,-1,-1],
 		[-1,1,-1,-1],
@@ -22,17 +22,27 @@ datasets = [
 		[-1,1,1,1],
 		[1,1,1,1]
 	]
+targets = [
+ [1, 1, 1, -1, 1, 1, -1, 1, 1, 1, -1, 1, -1, -1, -1, 1],
+ [1, 1, -1, 1, 1, 1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1],
+ [1, 1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1],
+ [1, 1, 1, -1, 1, 1, 1, 1, -1, -1, -1, 1, 1, 1, -1, 1],
+ [-1, -1, -1, 1, -1, 1, 1, 1, -1, 1, 1, -1, 1, 1, 1, -1],
+ [1, -1, 1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1]
+]
 weights = [0.,0.,0.]
 for i in range(len(weights)):
 	weights[i] = random.uniform(-0.2, 0.2)
+threshold = random.uniform(-1., 1.)
 learning_rate = 0.02
 
 def guess(inputs, weights):
 	total_activation = 0.
 	for input,weight in zip(inputs, weights):
 		total_activation+=input*weight
-	return 1 if total_activation >= 0 else -1
-
+	total_activation = total_activation - threshold
+	return np.tanh(0.5*total_activation)
+	
 def train(inputs, target, learning_rate, weights):
 	error = target - guess(inputs, weights)
 	#if error == 0: print("pas d erreur")
@@ -41,10 +51,13 @@ def train(inputs, target, learning_rate, weights):
 	return weights
 
 def main(weights, verbose=False):
-	for inputs in datasets:
-		for i in range(10000):
-			target = inputs[3]
+	targetsA = targets[0]
 
+	for inputs in datasets: #each inputs is a function
+		#Take the right target in A for the inputs
+		target = targetsA[datasets.index(inputs)]
+		
+		for i in range(10000):
 			old_weights = [0.,0.,0.]
 			for j in range(len(weights)):
 				old_weights[j] = weights[j]
@@ -56,8 +69,25 @@ def main(weights, verbose=False):
 
 	#Try a guess with good new updated weights	
 	for inputs in datasets:
-		print(guess(inputs, weights))
+		guessTamp = 0
+		target = targetsA[datasets.index(inputs)]
+		if guess(inputs, weights) >= 0:
+			guessTamp = 1
+		else:
+			guessTamp = -1
+		if target == guessTamp:
+			print("(+) Success")
+		else: 
+			print("(+) Fail")
+	print("---------")
+
+
 
 if __name__=='__main__':
-	main(weights)
-
+	for i in range(10):
+		weights = [0.,0.,0.]
+		for i in range(len(weights)):
+			weights[i] = random.uniform(-0.2, 0.2)
+		threshold = random.uniform(-1., 1.)
+		learning_rate = 0.02
+		main(weights)
