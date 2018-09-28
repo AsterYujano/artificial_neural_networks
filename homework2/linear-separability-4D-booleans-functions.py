1c56e629661f79478f4e7e743c0ecfcedd7ef7fe
François <asteryujano@gmail.com>
2018-09-28 13:36:25 +0100

    commit

--- homework2/linear-separability-4D-booleans-functions.py
+++ homework2/linear-separability-4D-booleans-functions.py
@@ -37,37 +37,40 @@ threshold = 0
 learning_rate = 0.02
 beta = 0.5
 
-def guess(inputs, weights):
+def guess(inputs, weights, threshold=threshold):
 	total_activation = 0.
-	threshold = random.uniform(-1., 1.) #todo : To update ?
 	for input,weight in zip(inputs, weights):
 		total_activation+=input*weight
 	total_activation = total_activation - threshold
 	return np.tanh(beta*total_activation)
 
 #Todo : chaque neuron a son Threshold, appliquer la formule de dtheta poru l'update
-def train(inputs, target, learning_rate, weights):
-	output = guess(inputs, weights)
+def train(inputs, target, learning_rate, weights, threshold):
+	output = guess(inputs, weights, threshold)
 	error = target - output
-	#update of weights
 	for i in range(len(weights)):
+		threshold += learning_rate * beta * error * (1 - (output)**2)
 		weights[i] += learning_rate * beta * error * (1 - output) * inputs[i] 
-	return weights
+	return weights, threshold
 
-def main(weights):
+def main(weights, verbose=False):
 	targetsA = targets[0]
 
-	for inputs in datasets:
-		#Take the right target in A for the inputs
-		target = targetsA[datasets.index(inputs)]
-		
-		for i in range(10000):
-			old_weights = [0.,0.,0.]
-			for j in range(len(weights)):
-				old_weights[j] = weights[j]
+	inputs = (datasets[random.randint(0, 15)])
+	if verbose: print("inputs chosen : "+str(inputs))
 
-			weights = train(inputs, target, learning_rate, weights)
-			if old_weights == weights: break
+	#Take the right target in A for the inputs
+	target = targetsA[datasets.index(inputs)]
+	threshold = random.uniform(-1., 1.) 
+	#print("pre : "+str(threshold))
+	for i in range(10000):
+		old_weights = [0.,0.,0.]
+		for j in range(len(weights)):
+			old_weights[j] = weights[j]
+
+		weights, threshold = train(inputs, target, learning_rate, weights, threshold)
+		#print("updated : "+str(threshold))
+		if old_weights == weights: break
 
 	#Try a guess with good new updated weights	
 	for inputs in datasets:
@@ -83,13 +86,13 @@ def main(weights):
 			print("(+) Fail")
 	print("---------")
 
-
-
 if __name__=='__main__':
-	for i in range(10):
-		weights = [0.,0.,0.]
-		for i in range(len(weights)):
-			weights[i] = random.uniform(-0.2, 0.2)
-		threshold = random.uniform(-1., 1.)
-		learning_rate = 0.02
-		main(weights)
+	weights = [0.,0.,0.]
+	for i in range(len(weights)):
+		weights[i] = random.uniform(-0.2, 0.2)
+	threshold = random.uniform(-1., 1.)
+	learning_rate = 0.02
+
+	for m in range(10): #todo 10^5
+
+		main(weights, verbose=True)
